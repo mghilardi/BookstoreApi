ba50a1b466eed5ca6e21f89b07a77f22b0ccd021
MDG <mauricio.ghilardi@gmail.com>
2024-10-02 21:58:52 +0200

    Add App exception handler

--- src/main/java/com/example/bookstore/api/exception/ErrorMessages.java
+++ src/main/java/com/example/bookstore/api/exception/ErrorMessages.java
@@ -1,8 +1,8 @@
 package com.example.bookstore.api.exception;
 
 public class ErrorMessages {
-    public static final String ERROR_BOOK_NOT_FOUND = "Book does not exist with id: ";
-    public static final String ERROR_BOOK_ALREADY_FOUND = "Book already exists with register id: ";
+    public static final String NOT_EXIST_WITH_ID = "Resource does not exist with id: ";
+    public static final String ERROR_BOOK_ALREADY_FOUND = "Resource already exists with id: ";
 
 
 }
